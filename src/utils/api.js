95e387e928d73981aac78e87bb9d4eac875c3bab
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-07-27 21:02:35 +0530

    Initial configuration for api hit

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+
+const headers = {
+    Authorization: `Bearer ${TMDB_TOKEN}`,
+};
+
+export const get = async (url, params = {}) => {
+    try {
+        const {data} = await axios.get(`${BASE_URL}${url}`, {
+            params,
+            headers,
+        });
+        return data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
