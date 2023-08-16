e1b84826e03ff22f1e9fb9f38f70d928819bd10d
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-08-16 07:51:02 +0530

    Fetched video and crew data

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import DetailsBanner from './detailsBanner/DetailsBanner';
+import useFetch from '../../hooks/useFetch';
 
 import "./style.scss";
 
 const Detail = () => {
+
+  const { mediaType, id } = useParams();
+  const { data, isLoading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, isLoading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
   return (
     <div>
-      
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
     </div>
   )
 }
