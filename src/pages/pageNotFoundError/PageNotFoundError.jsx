1209b0c9db12a6520166925378205616dfb0ddd5
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-08-17 18:06:36 +0530

    Created page not found component

--- src/pages/pageNotFoundError/PageNotFoundError.jsx
+++ src/pages/pageNotFoundError/PageNotFoundError.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+
 import "./style.scss";
 
 const PageNotFoundError = () => {
   return (
-    <div>
-      
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
     </div>
-  )
+  );
 }
 
 export default PageNotFoundError
